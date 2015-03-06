a761e72d99a8d1eaa14a35314af3b5648e153f5f
Kim Pettersen <kim@brightbytes.net>
2015-03-05 16:16:43 -0800

    Added __init__.py to gitignore to prevent exposing keys

--- populargithub/settings/__init__.py
+++ populargithub/settings/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
-from .settings import * 
+from .settings import *
 from django.conf import settings
 
 # Your GitHub OAUTH Token
 # See this url for obtaining one:
 # https://developer.github.com/v3/oauth/
-GITHUB_CLIENTID = '16ce2a1ee6d2e26c2b4c'
-GITHUB_CLIENTSECRET = '67c3370948cc3830cec5232bc1fd4703d7656057'
+GITHUB_CLIENTID = 'd905a6e97675802d4bfb'
+GITHUB_CLIENTSECRET = 'b300041c0a74638da782e6c0ef4d9acac7441b9f'
